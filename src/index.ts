b727735a437736eb151300917eb2084299b5ee2d
lingj <lingj.jhf@outlook.com>
2023-02-15 14:31:42 +0800

    添加options

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,17 @@ import { CSSEntries, Preset, UtilObject } from '@unocss/core'
 import browserslist from 'browserslist'
 import { transformStyleAttribute, browserslistToTargets } from 'lightningcss'
 
-export function autoPrefixPreset(): Preset {
+export interface Options {
+  browserslist: string[]
+}
+
+export function autoPrefixPreset(options: Options): Preset {
   return {
     name: 'unocss-preset-autoprefix',
     postprocess: (util: UtilObject) => {
       const { code } = transformStyleAttribute({
         code: Buffer.from(util.entries.map((item) => item.join(':')).join(';')),
-        targets: browserslistToTargets(browserslist(['>=0.05%'])),
+        targets: browserslistToTargets(browserslist(options.browserslist)),
       })
       util.entries = code
         .toString()
